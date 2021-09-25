d04a5ad53089a1f40fc304f1e6b008671869c0a2
webgrig <grigoriev@gmail.com>
2021-09-25 16:06:37 +0200

    CRUD users + greed + filter + sort + pagination

--- manager/src/Controller/Profile/OAuth/DetachController.php
+++ manager/src/Controller/Profile/OAuth/DetachController.php
@@ -25,7 +25,7 @@ class DetachController extends AbstractController
     }
 
     /**
-     * @Route("/detach/{network}/{identity}", name="profile.oauth.detach", methods={"DELETE"})
+     * @Route("/detach/{network}/{identity}", name="profile.oauth.detach", methods={"POST", "DELETE"})
      * @param Request $request
      * @param string $network
      * @param string $identity
--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -4,46 +4,31 @@ declare(strict_types=1);
 
 namespace App\ReadModel\User;
 
+use App\ReadModel\User\Filter\Filter;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\FetchMode;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 class UserFetcher
 {
     private $connection;
+    private $paginator;
 
-    public function __construct(Connection $connection)
+    public function __construct(Connection $connection, PaginatorInterface $paginator)
     {
         $this->connection = $connection;
+        $this->paginator = $paginator;
     }
 
     public function existsByResetToken(string $token): bool
     {
         return $this->connection->createQueryBuilder()
-            ->select('COUNT (*)')
-            ->from('user_users')
-            ->where('reset_token_token = :token')
-            ->setParameter(':token', $token)
-            ->execute()->fetchColumn(0) > 0;
-    }
-
-    public function findBySignUpConfirmToken(string $token): ?ShortView
-    {
-        $stmt = $this->connection->createQueryBuilder()
-            ->select(
-                'id',
-                'email',
-                'role',
-                'status'
-            )
-            ->from('user_users')
-            ->where('confirm_token = :token')
-            ->setParameter(':token', $token)
-            ->execute();
-
-        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, ShortView::class);
-        $result = $stmt->fetch();
-
-        return $result ?: null;
+                ->select('COUNT (*)')
+                ->from('user_users')
+                ->where('reset_token_token = :token')
+                ->setParameter(':token', $token)
+                ->execute()->fetchColumn(0) > 0;
     }
 
     public function findForAuthByEmail(string $email): ?AuthView
@@ -75,6 +60,7 @@ class UserFetcher
                 'u.id',
                 'u.email',
                 'u.password_hash',
+                'TRIM(CONCAT(u.name_first, \' \', u.name_last)) AS name',
                 'u.role',
                 'u.status'
             )
@@ -103,6 +89,26 @@ class UserFetcher
             ->from('user_users')
             ->where('email = :email')
             ->setParameter(':email', $email)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, ShortView::class);
+        $result = $stmt->fetch();
+
+        return $result ?: null;
+    }
+
+    public function findBySignUpConfirmToken(string $token): ?ShortView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'email',
+                'role',
+                'status'
+            )
+            ->from('user_users')
+            ->where('confirm_token = :token')
+            ->setParameter(':token', $token)
             ->execute();
 
         $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, ShortView::class);
@@ -153,5 +159,55 @@ class UserFetcher
             throw new \LogicException('User is not found');
         }
         return $detail;
+    }
+
+    /**
+     * @param Filter $filter
+     * @param int $page
+     * @param int $size
+     * @param string $sort
+     * @param string $direction
+     * @return PaginationInterface
+     */
+    public function all(Filter $filter, int $page, int $size, string $sort, string $direction): PaginationInterface
+    {
+        $qb = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'date',
+                'TRIM(CONCAT(name_first, \' \', name_last)) AS name',
+                'email',
+                'role',
+                'status'
+            )
+            ->from('user_users');
+
+        if ($filter->name) {
+            $qb->andWhere($qb->expr()->like('LOWER(CONCAT(name_first, \' \', name_last))', ':name'));
+            $qb->setParameter(':name', '%' . mb_strtolower($filter->name) . '%');
+        }
+
+        if ($filter->email) {
+            $qb->andWhere($qb->expr()->like('LOWER(email)', ':email'));
+            $qb->setParameter(':email', '%' . mb_strtolower($filter->email) . '%');
+        }
+
+        if ($filter->status) {
+            $qb->andWhere('status = :status');
+            $qb->setParameter(':status', $filter->status);
+        }
+
+        if ($filter->role) {
+            $qb->andWhere('role = :role');
+            $qb->setParameter(':role', $filter->role);
+        }
+
+        if (!\in_array($sort, ['date', 'name', 'email', 'role', 'status'], true)) {
+            throw new \UnexpectedValueException('Cannot sort by ' . $sort);
+        }
+
+        $qb->orderBy($sort, $direction === 'desc' ? 'desc' : 'asc');
+
+        return $this->paginator->paginate($qb, $page, $size);
     }
 }
