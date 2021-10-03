a945e882e1e9ff5fb26557a769afcac5df3d95ef
webgrig <grigoriev@gmail.com>
2021-10-03 12:30:17 +0200

    modified css & js

--- manager/assets/app.js
+++ manager/assets/app.js
@@ -15,3 +15,18 @@ import './images/coreui.svg';
 // start the Stimulus application
 
 import './bootstrap';
+
+const Centrifuge = require('centrifuge');
+const toastr = require('toastr');
+
+document.addEventListener('DOMContentLoaded', function () {
+    let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
+    let user = document.querySelector('meta[name=centrifugo-user]').getAttribute('content');
+    let token = document.querySelector('meta[name=centrifugo-token]').getAttribute('content');
+    let centrifuge = new Centrifuge(url);
+    centrifuge.setToken(token);
+    centrifuge.subscribe('alerts#' + user, function (message) {
+        toastr.info(message.data.message);
+    });
+    centrifuge.connect();
+});
